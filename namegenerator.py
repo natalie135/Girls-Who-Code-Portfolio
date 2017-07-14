29d2b00c84d2fcf27eb752375f79d965e2a62a3c
natalie135 <nnatalieaustin@gmail.com>
2017-07-14 14:54:20 -0700

    adding obamicon

---
+++ namegenerator.py
@@ -0,0 +1,5 @@
+names = ["Carina", "Sara", "Jessica", "Melinda", "Maria", "Tegan", "Grace", "Gary", "Cameron", "Sophie"]
+print("Would you like a name?")
+user_input = input("yes")
+if user_input = "yes"
+    print(random.choice(names))
---
+++ obamicon.py
@@ -0,0 +1,38 @@
+from PIL import Image
+
+# RGB values for recoloring.
+darkBlue = (0, 51, 76)
+red = (217, 26, 33)
+lightBlue = (112, 150, 158)
+yellow = (252, 227, 166)
+
+# Import image.
+my_image = Image.open("gma.jpg")#change IMAGENAME to the path on your computer to the image you're using
+my_image.show() #showing the image
+image_list = my_image.getdata() #each pixel is represented in the form (red value, green value, blue value, transparency). You don't need the fourth value.
+image_list = list(image_list) #Turns the sequence above into a list. The list can be iterated through in a l
+
+recolored = [] #list that will hold the pixel data for the new image.
+
+for pixel in image_list:
+    s = sum(pixel)
+    if s < 182:
+        recolored.append(darkBlue)
+    elif s < 364:
+        recolored.append(red)
+    elif s < 546:
+        recolored.append(lightBlue)
+    else:
+        recolored.append(yellow)
+
+
+
+#YOUR CODE to loop through the original list of pixels and build a new list based on intensity should go here.
+
+
+
+# Create a new image using the recolored list. Display and save the image.
+new_image = Image.new("RGB", my_image.size) #Creates a new image that will be the same size as the original image.
+new_image.putdata(recolored) #Adds the data from the recolored list to the image.
+new_image.show() #show the new image on the screen
+new_image.save("recolored.jpg", "jpeg") #save the new image as "recolored.jpg"
